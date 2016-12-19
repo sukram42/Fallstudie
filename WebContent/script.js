4411861393676efbe68d6a6b10da7ca772ba5ddd
Markus <markus.boebel@hpe.com>
2016-12-19 01:15:39 +0100

    Improve Userinterface test Just bootstrapped dat shiet

--- WebContent/script.js
+++ WebContent/script.js
@@ -36,6 +36,22 @@ function addEmployee(anzahl)
         }),
         dataType: 'json'
     });
-
 }
+function createCompany()
+{
+    var data = JSON.stringify({"passwort":"passwort","name":"hallo GmbG","fremdkapital":100.0,"eigenkapital":1000.0,"hr":{"mitarbeiter":[]}});
+    $.ajax({
+        type: 'POST',
+        url: 'http://localhost:8080/Fallstudie-0.0.1-SNAPSHOT/rest/companies/',
+        data: data,
+        success: (function (datan) {
+            alert(datan);
+        }),
+        contentType: "application/json",
+        error: (function (a,b,c) {
+            alert("Error " + b + " " +c );
+        }),
+    });
+}
+
 
