e4bf5b3b08fc51cb9cfcf14aa7c470fe3a1c2755
Luca Dommes <luca.dommes@hpe.com>
2016-12-20 19:28:41 +0100

    added MongoDB insertion to the addMitarbeiter-method

--- src/Unternehmen/Abteilung.java
+++ src/Unternehmen/Abteilung.java
@@ -4,7 +4,12 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
+import javax.print.Doc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -41,6 +46,11 @@ public class Abteilung {
 		JsonObject object = new Gson().fromJson(erg, JsonObject.class);
 		JsonArray array = object.getAsJsonArray("results");
 
+		// preparing MongoDB for insertion:
+        MongoClient mongo = new MongoClient("localhost", 27017);
+        MongoDatabase db = mongo.getDatabase("Fallstudie");
+        MongoCollection<Document> coll = db.getCollection("mitarbeiter");
+
 		for (int i = 0; i < array.size(); i++) {
 			JsonObject name = (JsonObject) array.get(i).getAsJsonObject().get("name");
 			JsonObject address = (JsonObject) array.get(i).getAsJsonObject().get("location");
@@ -50,6 +60,14 @@ public class Abteilung {
 					address.get("street").getAsString() + " " + address.get("city").getAsString(),
 					picture.get("medium").getAsString(), 'w');
 			System.out.println(m.getVorname() + " " + m.getName());
+
+			// inserting m into the MongoDB:
+            Document doc = new Document("name", m.getName())
+                    .append("vorname", m.getVorname())
+                    .append("adresse", m.getAdresse())
+                    .append("imagelink", m.getImagelink())
+                    .append("gender", m.getGender());
+            coll.insertOne(doc);
 
 			mitarbeiter.add(m);
 
