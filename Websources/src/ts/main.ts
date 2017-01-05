a49f11cdbc5b259e00661287676c8045469c128e
Markus <markus.boebel@hpe.com>
2017-01-05 02:01:40 +0100

    Login
    Improving Login
    Implementing Creation of new Companies

--- Websources/src/ts/main.ts
+++ Websources/src/ts/main.ts
@@ -2,6 +2,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 
 import {AppModule} from "./modules/app.module";
-
 platformBrowserDynamic().bootstrapModule(AppModule);
 
