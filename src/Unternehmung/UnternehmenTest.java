8a5a36374bae52326c34a0fddb5e6319c6c4642e
Luca Dommes <luca.dommes@hpe.com>
2017-01-08 18:56:54 +0100

    Rules-Package um Spielmechanik zu implementieren Game-Klasse u.a. zur Ermittlung des Gewinners

--- src/Unternehmung/UnternehmenTest.java
+++ src/Unternehmung/UnternehmenTest.java
@@ -1,5 +1,6 @@
 package Unternehmung;
 
+import Rules.Game;
 import Unternehmung.Abteilungen.Marketing;
 import Unternehmung.Abteilungen.Produktion;
 import Unternehmung.Abteilungen.Vertrieb;
@@ -11,8 +12,9 @@ import Unternehmung.Abteilungen.Vertrieb;
 public class UnternehmenTest {
     public static void main(String[] args) {
 
+        // ############################## UNTERNEHMEN 1 ##############################
         // Unternehmen erstellen:
-        Unternehmen unternehmen1 = new Unternehmen("TestCo1", "kennwort", 100000, 100000);
+        Unternehmen unternehmen1 = new Unternehmen("Unternehmen 1", "kennwort", 100000, 100000);
 
         System.out.println("Unternehmen " + unternehmen1.getName() + " mit Eigenkapital in Höhe von " +
                 unternehmen1.getKennzahlen().getEigenkapital() + " und Fremdkapital in Höhe von " +
@@ -23,23 +25,57 @@ public class UnternehmenTest {
         unternehmen1.getAbteilung("vertrieb").addMitarbeiter(2, 35000);
 
         // Maschinen kaufen:
-        Produktion produktion = (Produktion) unternehmen1.getAbteilung("produktion");
-        produktion.maschinenKaufen("3", 2);
+        Produktion produktion1 = (Produktion) unternehmen1.getAbteilung("produktion");
+        produktion1.maschinenKaufen("3", 2);
 
         // Rucksäcke produzieren:
-        produktion.produzieren("Rucksack", 6000, 10);
+        produktion1.produzieren("Rucksack", 6000, 10);
 
         // Marketingkampagne und Marktforschung:
-        Marketing marketing = (Marketing) unternehmen1.getAbteilung("marketing");
-        marketing.marketingKampagne("Print");
-        marketing.marketingKampagne("TV");
-        marketing.marktforschung(2);
+        Marketing marketing1 = (Marketing) unternehmen1.getAbteilung("marketing");
+        marketing1.marketingKampagne("Print");
+        marketing1.marketingKampagne("TV");
+        marketing1.marktforschung(2);
 
         // Rucksäcke verkaufen:
-        Vertrieb vertrieb = (Vertrieb) unternehmen1.getAbteilung("vertrieb");
-        vertrieb.verkaufen("Rucksack", 50);
+        Vertrieb vertrieb1 = (Vertrieb) unternehmen1.getAbteilung("vertrieb");
+        vertrieb1.verkaufen("Rucksack", 50);
 
-        // Jahresende:
+
+        // ############################## UNTERNEHMEN 2 ##############################
+        // Unternehmen erstellen:
+        Unternehmen unternehmen2 = new Unternehmen("Unternehmen 2", "kennwort", 100000, 100000);
+
+        System.out.println("Unternehmen " + unternehmen2.getName() + " mit Eigenkapital in Höhe von " +
+                unternehmen2.getKennzahlen().getEigenkapital() + " und Fremdkapital in Höhe von " +
+                unternehmen2.getKennzahlen().getFremdkapital() + " gegründet.");
+
+        // Mitarbeiter einstellen:
+        unternehmen2.getAbteilung("produktion").addMitarbeiter(2, 25000);
+        unternehmen2.getAbteilung("vertrieb").addMitarbeiter(2, 35000);
+
+        // Maschinen kaufen:
+        Produktion produktion2 = (Produktion) unternehmen2.getAbteilung("produktion");
+        produktion2.maschinenKaufen("3", 3);
+
+        // Rucksäcke produzieren:
+        produktion2.produzieren("Rucksack", 9000, 15);
+
+        // Marketingkampagne und Marktforschung:
+        Marketing marketing2 = (Marketing) unternehmen2.getAbteilung("marketing");
+        marketing2.marketingKampagne("Plakate");
+        marketing2.marketingKampagne("Print");
+        marketing2.marketingKampagne("Radio");
+        marketing2.marketingKampagne("TV");
+        marketing2.marktforschung(3);
+
+        // Rucksäcke verkaufen:
+        Vertrieb vertrieb2 = (Vertrieb) unternehmen2.getAbteilung("vertrieb");
+        vertrieb2.verkaufen("Rucksack", 50);
+
+
+        // ############################## JAHRESENDE ##############################
+        // Unternehmen 1:
         System.out.println("Das Unternehmen " + unternehmen1.getName() + " hat in diesem Geschäftsjahr ein Ergebnis von " +
                 unternehmen1.getKennzahlen().getGewinn() + " € erzielt.");
         System.out.println("Kosten: " + (unternehmen1.getKennzahlen().getSonstigeKosten() + unternehmen1.getKennzahlen().getGehälter() +
@@ -47,6 +83,19 @@ public class UnternehmenTest {
         System.out.println("Umsatz: " + unternehmen1.getKennzahlen().getUmsatz());
         System.out.println("Bekanntheitsgrad: " + unternehmen1.getKennzahlen().getBekanntheitsgrad());
         System.out.println("Verkaufsrate: " + unternehmen1.getKennzahlen().getVerkaufsrate());
+
+        // Unternehmen 2:
+        System.out.println("Das Unternehmen " + unternehmen2.getName() + " hat in diesem Geschäftsjahr ein Ergebnis von " +
+                unternehmen2.getKennzahlen().getGewinn() + " € erzielt.");
+        System.out.println("Kosten: " + (unternehmen2.getKennzahlen().getSonstigeKosten() + unternehmen1.getKennzahlen().getGehälter() +
+                unternehmen2.getKennzahlen().getHerstellkosten()));
+        System.out.println("Umsatz: " + unternehmen2.getKennzahlen().getUmsatz());
+        System.out.println("Bekanntheitsgrad: " + unternehmen2.getKennzahlen().getBekanntheitsgrad());
+        System.out.println("Verkaufsrate: " + unternehmen2.getKennzahlen().getVerkaufsrate());
+
+        // Gewinner ermitteln:
+        Game game = new Game(unternehmen1, unternehmen2);
+        System.out.println(game.gewinnerErmitteln().getName() + " ist der Gewinner!");
 
     }
 }
