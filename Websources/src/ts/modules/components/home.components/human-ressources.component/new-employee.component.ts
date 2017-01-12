cf419a9fef15789f7ef3639708ce75f860162916
Markus <markus.boebel@hpe.com>
2017-01-12 21:48:52 +0100

    Verbessern des Timers; Hinzufügen des Neuen Mitarbeiter Component
    Der Timer kann ein Datum, welches er darstellt,  zurückgeben: Ein Timercount ist dabei Ein Tag.
    
    Im Human Resources Component wurde das Modal an Angular JS angepasst

---
+++ Websources/src/ts/modules/components/home.components/human-ressources.component/new-employee.component.ts
@@ -0,0 +1,30 @@
+/**
+ * Created by boebel on 12.01.2017.
+ */
+/**
+ * Created by boebel on 09.01.2017.
+ */
+
+/**
+ * Created by boebel on 04.01.2017.
+ */
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'new-employee-modal',
+    templateUrl: '../../../../templates/components/home.components/human-ressources.component/new-employee.component.html',
+
+})
+
+export class NewEmployeeComponent {
+
+    newEmployee  = {}
+
+    saveEmployee()
+    {
+
+    }
+
+
+}
