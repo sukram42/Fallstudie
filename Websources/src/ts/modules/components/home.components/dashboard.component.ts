aa9d9e72be0e69c89acb7c585a2a60cc68508f9e
schusten <niklas.schuster@hpe.com>
2017-01-18 16:43:42 +0100

    create neues dashboard with  very cool Diagrammen

--- Websources/src/ts/modules/components/home.components/dashboard.component.ts
+++ Websources/src/ts/modules/components/home.components/dashboard.component.ts
@@ -17,16 +17,67 @@ import {Component} from '@angular/core';
 export class DashboardComponent {
     type = 'radar';
     data = {
-        labels: ["Mitarbeiterzufriedenheit", "Kundenzufriedenheit", "Bekanntheitsgrad","Image"],
+        labels: ["Mitarbeiterzufriedenheit", "Kundenzufriedenheit", "Bekanntheitsgrad","Image","Produktqualität"],
         datasets: [
             {
                 label: "Kennzahlen",
-                data: [65, 59, 80, 81]
+                backgroundColor: "rgba(255,173,41,0.7)",
+                borderColor: "rgba(255,173,41,1)",
+                data: [65, 59, 80, 81, 50]
+            },
+            {
+                label: "kennzahlen des letzten Geschäftsjahres",
+                backgroundColor: "rgba(49,53,61,0.7)",
+                borderColor: "rgba(49,53,61,1)",
+                data: [40, 60, 55, 31,35]
             }
         ]
     };
     options = {
         responsive: true,
-        maintainAspectRatio: false
+        maintainAspectRatio: false,
+        defaultFontSize: 100,
+    };
+
+    type2 = 'bar';
+    data2 = {
+        labels: ["Jan 2010", "Feb 2010", "Mär 2010","Apr 2010","Mai 2010","Jun 2010","Jul 2010","Aug 2010"],
+        datasets: [
+            {
+                label: "Erträge",
+                backgroundColor: "rgba(255,173,41,0.7)",
+                borderColor: "rgba(255,173,41,1)",
+                data: [100, 30, 20, 100,23 ,54, 60, 90]
+            },
+            {
+                label: "Aufwendungen",
+                backgroundColor: "rgba(49,53,61,0.7)",
+                borderColor: "rgba(49,53,61,1)",
+                data: [40, 60, 55, 31, 49, 30, 80, 48]
+            }
+        ]
+    };
+    options2 = {
+        responsive: true,
+        maintainAspectRatio: false,
+        defaultFontSize: 100,
+    };
+
+    type3 = 'polarArea';
+    data3 = {
+        labels: ["Test AG", "Test AG 2", "Test AG 3"],
+        datasets: [
+            {
+                label: "Erträge",
+                backgroundColor: ["rgba(255,173,41,0.7)","rgba(49,35,61,0.7)","rgba(28,29,33,0.7)"],
+                borderColor: ["rgba(255,173,41,1)","rgba(49,35,61,1)","rgba(28,29,33,1)"],
+                data: [60, 30, 10]
+            }
+        ]
+    };
+    options3 = {
+        responsive: true,
+        maintainAspectRatio: false,
+        defaultFontSize: 100,
     };
 }
