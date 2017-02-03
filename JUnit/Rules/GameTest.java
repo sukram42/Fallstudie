cf86533a5e916f9a73c466428194d9c2b40ecc55
knattle <jan_malte@t-online.de>
2017-02-04 00:15:27 +0100

    Merge branch 'master' of C:\Users\d064018\IdeaProjects\Fallstudie_Kopie with conflicts.

--- JUnit/Rules/GameTest.java
+++ JUnit/Rules/GameTest.java
@@ -1,5 +1,5 @@
 package Rules;
-//
+
 import Unternehmung.Abteilungen.Produktion;
 import Unternehmung.Kennzahlensammlung;
 import Unternehmung.Unternehmen;
@@ -13,13 +13,6 @@ import java.util.GregorianCalendar;
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-//
-
-import Rules.Game;
-import Unternehmung.Abteilungen.Produktion;
-import Unternehmung.Kennzahlensammlung;
-import Unternehmung.Unternehmen;
-import org.junit.Before;
 
 /**
  * Created by D064018 on 27.01.2017.
@@ -40,7 +33,7 @@ public class GameTest{
         testGame.getCompanies().add(unternehmen);
         assertNotNull(testGame);
     }
-//
+
     @Test
     public void getCompanies() throws Exception {
         assertTrue(Game.getCompanies().contains(unternehmen));
@@ -55,39 +48,39 @@ public class GameTest{
     public void getTime() throws Exception {
 assertNotNull(Game.getTime());
     }
-//
-//    @Test
-//    public void getTimeString() throws Exception {
-//assertNotNull(Game.getTimeString());
-//    }
-//
-//    @Test
-//    public void getCalendar() throws Exception {
-//        assertNotNull(Game.getCalendar());
-//    }
-//
-//    @Test
-//    public void getAusschreibungen() throws Exception {
-//        assertTrue(Game.getAusschreibungen().size() == 0);
-//    }
-//
-//    @Test
-//    public void _run() throws Exception {
-//        testGame.run();
-//        assertNotNull(Game.getAusschreibungen());
-//    }
-//
-//    @Test
-//    public void updateCounter() throws Exception {
-//    testGame.updateCounter();
-//        Calendar calendar = new GregorianCalendar(2010, 1, 2);
-//        assertEquals(Game.getCalendar(), calendar); //angeblich sei calender == 2012, 2, 3
-//    }
-//
-//    @After
-//    public void tearDown(){
-//        System.setOut(null);
-//        System.setErr(null);
-//    }
-//
+
+    @Test
+    public void getTimeString() throws Exception {
+assertNotNull(Game.getTimeString());
+    }
+
+    @Test
+    public void getCalendar() throws Exception {
+        assertNotNull(Game.getCalendar());
+    }
+
+    @Test
+    public void getAusschreibungen() throws Exception {
+        assertTrue(Game.getAusschreibungen().size() == 0);
+    }
+
+    @Test
+    public void _run() throws Exception {
+        testGame.run();
+        assertNotNull(Game.getAusschreibungen());
+    }
+
+    @Test
+    public void updateCounter() throws Exception {
+    testGame.updateCounter();
+        Calendar calendar = new GregorianCalendar(2010, 1, 2);
+        assertEquals(Game.getCalendar(), calendar); //angeblich sei calender == 2012, 2, 3
+    }
+
+    @After
+    public void tearDown(){
+        System.setOut(null);
+        System.setErr(null);
+    }
+
 }
